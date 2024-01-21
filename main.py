2b54b74dafe74019dd7238d02953df7279368a0d
Paul <pavelkarastoynov@gmail.com>
2024-01-21 13:34:37 +0200

    Version 2.0

---
+++ main.py
@@ -0,0 +1,31 @@
+#class Dmitriy:
+#    group = "Class - 8-A"
+#    gender = "Gender - Male"
+#    education = "School - Middle School of Ivan Vazov"
+
+#    def __init__(self):
+#        self.name = "Name - Dmitriy Ivanov"
+#        self.age = "Age - 14 Years Old"
+
+#class Nastya:
+#    group = "Class - 10-B"
+#    gender = "Gender - Female"
+#    education = "School - Medical College"
+#
+#    def __init__(self):
+#        self.name = "Name - Nastya Olegovna"
+#        self.age = "Age - 16 Years Old"
+
+#st1 = Dmitriy()
+#print(st1.name)
+#print(st1.age)
+#st2 = Nastya()
+#print(st2.name)
+#print(st2.age)
+
+#print (st1.group)
+#print (st1.gender)
+#print (st1.education)
+#print (st2.group)
+#print (st2.gender)
+#print (st2.education)
